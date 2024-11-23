0a07434f2fefa32976e6ee78b8904f791f8bdcc1
Wilinton Gabriel <wilintongabrielss1@gmail.com>
2024-11-23 14:27:52 -0300

    Criação do rodapé

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Cabecalho from './components/Cabecalho/Cabecaho'
+import Rodape from './components/Rodape/Rodape'
 
 function App() {
 
   return (
     <>
       <Cabecalho />
+      <Rodape />
     </>
   )
 }
