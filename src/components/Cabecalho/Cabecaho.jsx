c86617a90edf9291c665841830b2c5d148e252a7
Wilinton Gabriel <wilintongabrielss1@gmail.com>
2024-11-23 11:21:47 -0300

    Correção: scroll lateral no mobile

--- src/components/Cabecalho/Cabecaho.jsx
+++ src/components/Cabecalho/Cabecaho.jsx
@@ -2,12 +2,12 @@ import Logo from "../Logo/Logo";
 
 export default function Cabecalho() {
     return (
-        <header className="text-white font-bold flex justify-between m-2 p-3">
+        <header className="text-white font-bold flex justify-between p-1">
             <Logo />
             <nav className="p-3">
-                <a href="#" className="mx-3 lg:mx-6 text-base lg:text-lg transition-all duration-300 ease-in-out hover:text-red-500 hover:scale-105 hover:underline">Início</a>
-                <a href="#" className="mx-3 lg:mx-6 text-base lg:text-lg transition-all duration-300 ease-in-out hover:text-red-500 hover:scale-105 hover:underline">Projetos</a>
-                <a href="#" className="mx-3 lg:mx-6 text-base lg:text-lg transition-all duration-300 ease-in-out hover:text-red-500 hover:scale-105 hover:underline">Contato </a>
+                <a href="#" className="mx-3 lg:mx-6 text-base lg:text-lg md:text-sm transition-all duration-300 ease-in-out hover:text-red-500 hover:scale-105 hover:underline">Início</a>
+                <a href="#" className="mx-3 lg:mx-6 text-base lg:text-lg md:text-sm transition-all duration-300 ease-in-out hover:text-red-500 hover:scale-105 hover:underline">Projetos</a>
+                <a href="#" className="mx-3 lg:mx-6 text-base lg:text-lg md:text-sm transition-all duration-300 ease-in-out hover:text-red-500 hover:scale-105 hover:underline">Contato </a>
             </nav>
         </header>
     );
