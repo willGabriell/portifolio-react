0ce7d2cb7650a8c062c88f606c43067a996a0c1b
Wilinton Gabriel <wilintongabrielss1@gmail.com>
2024-11-22 23:26:10 -0300

    Correção: Logo quebrada no mobile

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,10 @@
 export default function Logo() {
     return (
-        <div className="flex flex-col items-center p-3 cursor-pointer">
-            <h1 className="text-xl lg:text-xl font-bold transition-all duration-300 ease-in-out hover:text-red-500 hover:scale-105">
+        <div className="flex flex-col items-center text-center p-3 cursor-pointer">
+            <h1 className="text-lg md:text-xl font-bold transition-all duration-300 ease-in-out hover:text-red-500 hover:scale-105 whitespace-nowrap">
                 Wilinton Gabriel
             </h1>
-            <p className="text-sm lg:text-base font-light text-gray-300">
+            <p className="text-xs md:text-sm font-light text-gray-300">
                 Desenvolvedor <span className="text-red-500 font-bold">Java</span>
             </p>
         </div>
