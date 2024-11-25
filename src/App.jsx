d1f274651cbef74de477f96ccab2cbbee756e38d
Wilinton Gabriel <wilintongabrielss1@gmail.com>
2024-11-25 11:10:54 -0300

    Sessão "Início" criada
    sessão criada mas ainda não finalizada
    foi feita apenas uma metade dela

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Cabecalho from './components/Cabecalho/Cabecaho'
+import Inicio from './components/Inicio/Inicio'
 import Projetos from './components/Projetos/Projetos'
 import Rodape from './components/Rodape/Rodape'
 
@@ -9,6 +10,7 @@ function App() {
     <div className="app-container">
       <Cabecalho />
       <div className="main-content">
+        <Inicio />
         <Projetos />
       </div>
       <Rodape />
